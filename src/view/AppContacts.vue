a9dd32f7abcc60192119503c85b75bc5cda151dc
chrialge <chrialge@gmail.com>
2024-06-05 01:06:23 +0200

    add restyle of the form of contacts

--- src/view/AppContacts.vue
+++ src/view/AppContacts.vue
@@ -17,49 +17,46 @@ export default {
 <template>
     <Jumbotron btnName="My Home" btnRoute="home" />
     <div class="container py-10">
+        <h2 class="title">CREDENTIALS</h2>
+        <span class="sub_title">Insert your credential for contact me!</span>
 
 
 
 
-        <h1 class="text-black">Insert your credential for contact me</h1>
+
+        <form action="" class="py-10 flex flex-col">
 
 
-        <form action="" class="flex flex-col gap-4 py-2">
-            <label class="input input-bordered flex items-center gap-2">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
-                    class="w-4 h-4 opacity-70">
-                    <path
-                        d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
-                </svg>
-                <input type="text" class="grow" placeholder="Mario" name="first_name"
-                    aria-describedby="first_nameHelperId" />
-                <small id="first_nameHelper"></small>
-            </label>
+            <div class="mb-5">
+                <label for="first-name" class="block mb-2 text-md font-medium sub_title">
+                    First name</label>
+                <input type="first-name" id="first-name"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Mario" required />
+            </div>
+            <div class="mb-5">
+                <label for="last-name" class="block mb-2 text-md font-medium sub_title">
+                    Last name</label>
+                <input type="last-name" id="last-name"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Rossi" required />
+            </div>
+            <div class="mb-5">
+                <label for="email" class="block mb-2 text-md font-medium sub_title">
+                    Email</label>
+                <input type="email" id="email"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 gg"
+                    placeholder="name@flowbite.com" required />
+            </div>
+            <div class="mb-5">
+                <label for="message" class="block mb-2 text-md font-medium sub_title">
+                    Message</label>
+                <textarea id="message" rows="4"
+                    class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Leave a comment..."></textarea>
+            </div>
 
-            <label class="input input-bordered flex items-center gap-2">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
-                    class="w-4 h-4 opacity-70">
-                    <path
-                        d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
-                </svg>
-                <input type="text" class="grow" placeholder="Rossi" name="last_name" />
-            </label>
-
-
-            <label class="input input-bordered flex items-center gap-2">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
-                    class="w-4 h-4 opacity-70">
-                    <path
-                        d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
-                    <path
-                        d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
-                </svg>
-                <input type="text" class="grow" placeholder="Mario@Rossi.com" name="email" />
-            </label>
-
-            <textarea class="textarea textarea-bordered" placeholder="message" rows="6" style="width: 100;"></textarea>
-
-            <button class="btn btn-primary">Send</button>
+            <button class="btn btn-primary my-8" style="background-color: white;">Send</button>
         </form>
     </div>
 
